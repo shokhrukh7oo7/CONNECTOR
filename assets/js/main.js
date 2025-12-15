db25aec5e71ab7cfc98362087750c40a4da232a9
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-12-15 21:17:58 +0500

    slider finished

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  // Swiper: Slider
+  new Swiper(".swiper-container", {
+    loop: true,
+    nextButton: ".swiper-button-next",
+    prevButton: ".swiper-button-prev",
+    slidesPerView: 4.5,
+    paginationClickable: true,
+    spaceBetween: 20,
+    breakpoints: {
+      1420: {
+        slidesPerView: 4.5,
+        spaceBetween: 30,
+      },
+      1028: {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 1.5,
+        spaceBetween: 10,
+      },
+    },
+  });
+});
