bc090924c04384f01d2f90eccde77691271c0328
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-12-16 13:41:54 +0500

    header burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger-btn');
+  const menu = document.querySelector('.mobile-menu');
+
+  if (!burger || !menu) return;
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+  });
+  const closeBtn = document.querySelector('.mobile-close');
+  
+  closeBtn.addEventListener('click', () => {
+    menu.classList.remove('active');
+    burger.classList.remove('active');
+  });
+});
+
 // --------------------------------------------
 $(document).ready(function () {
   // Swiper: Slider
