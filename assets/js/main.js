911e8bd21e74ac73ea4e5f5f40c980be3eb74d63
shokhrukh7oo7 <shokh7oo70809@gmail.com>
2025-12-17 15:18:14 +0500

    partner swiper finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burger = document.querySelector('.burger-btn');
-  const menu = document.querySelector('.mobile-menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger-btn");
+  const menu = document.querySelector(".mobile-menu");
 
   if (!burger || !menu) return;
 
-  burger.addEventListener('click', () => {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
+  burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
   });
-  const closeBtn = document.querySelector('.mobile-close');
-  
-  closeBtn.addEventListener('click', () => {
-    menu.classList.remove('active');
-    burger.classList.remove('active');
+  const closeBtn = document.querySelector(".mobile-close");
+
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove("active");
+    burger.classList.remove("active");
   });
 });
 
@@ -47,8 +47,46 @@ $(document).ready(function () {
   });
 });
 // ================================================
-document.addEventListener('DOMContentLoaded', () => {
-  const counter = document.querySelector('.counter');
+// partner swiper
+$(document).ready(function () {
+  // Swiper: Slider
+  new Swiper(".swiper-container-partner", {
+    loop: true,
+    slidesPerView: 10,
+    nextButton: ".swiper-button-next",
+    prevButton: ".swiper-button-prev",
+    autoplay: 3000, // ⏱ 4 секунды
+    autoplayDisableOnInteraction: false, // не останавливается после клика
+    paginationClickable: true,
+    spaceBetween: 20,
+    breakpoints: {
+      1920: {
+        slidesPerView: 10,
+        spaceBetween: 30,
+      },
+      1028: {
+        slidesPerView: 8,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      395: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+    },
+  });
+});
+
+// ================================================
+document.addEventListener("DOMContentLoaded", () => {
+  const counter = document.querySelector(".counter");
   if (!counter) return;
 
   const startYear = parseInt(counter.dataset.startYear, 10);
@@ -56,12 +94,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let started = false;
 
-  const observer = new IntersectionObserver(entries => {
-    if (entries[0].isIntersecting && !started) {
-      started = true;
-      animateCounter(counter, targetValue);
-    }
-  }, { threshold: 0.5 });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting && !started) {
+        started = true;
+        animateCounter(counter, targetValue);
+      }
+    },
+    { threshold: 0.5 }
+  );
 
   observer.observe(counter);
 });
